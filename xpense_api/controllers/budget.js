0e2cdc2f08c015fde36aa1148a83bab56bf37c88
Travis Southard <tsouthard88@gmail.com>
2020-07-22 17:04:39 -0400

    Added mongoose routes to all the routes. This should be complete.

--- xpense_api/controllers/budget.js
+++ xpense_api/controllers/budget.js
@@ -2,21 +2,43 @@
 const express = require("express");
 const budgets = express.Router();
 
+const Budget = require("../models/budget.js")
+
 //ROUTES
 budgets.get("/", (req, res) => {
-    res.send("Hey the index route works!");
+    Budget.find({}, (err, foundBudgets) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(foundBudgets);
+    });
 });
 
 budgets.post("/", (req, res) => {
-    res.send(req.body);
+    Budget.create(req.body, (err, createdBudget) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(createdBudget);
+    });
 });
 
 budgets.put("/:id", (req, res) => {
-    res.send("The update route works, but doesn't do anything yet.");
+    Budget.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedBudget) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(updatedBudget);
+    })
 });
 
 budgets.delete("/:id", (res, res) => {
-    res.send("The delete route works, but doesn't do anything yet.")
+    Budget.findByIdAndDelete(req.params.id, (err, deletedBudget) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(deletedBudget);
+    })
 })
 
 //EXPORTS
