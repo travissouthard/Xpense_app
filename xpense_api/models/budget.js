c98aa739280284b8ca6240b70a67c5c36c5219be
Travis Southard <tsouthard88@gmail.com>
2020-07-23 11:29:09 -0400

    Created seed file and adjusted budget schema so that budget.transactions defaults to an array

--- xpense_api/models/budget.js
+++ xpense_api/models/budget.js
@@ -4,7 +4,7 @@ const budgetSchema = mongoose.Schema({
     title: {type: String, required: true},
     budget: {type: Number, default: 0},
     spent: {type: Number, default: 0},
-    transactions: {type: String},
+    transactions: [{type: String}],
 })
 
 module.exports = mongoose.model('Budget', budgetSchema)
---
+++ xpense_api/models/budget_seed.js
@@ -0,0 +1,44 @@
+module.export = [
+    {
+        title: "Gas",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+    {
+        title: "Food",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+    {
+        title: "Lodging",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+    {
+        title: "Entertainment",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+    {
+        title: "Shopping",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+    {
+        title: "Car rental",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+    {
+        title: "Misc.",
+        budget: 0,
+        spent: 0,
+        transactions: [],
+    },
+]
