9bb018ad02a5e982936cf1220741ee01cf1132c4
Travis Southard <tsouthard88@gmail.com>
2020-07-23 12:19:44 -0400

    Updated budget categories seed to include a userId and adjusted the budget create route to pass the userId in and seed a budget for that user

--- xpense_api/models/budget_seed.js
+++ xpense_api/models/budget_seed.js
@@ -1,44 +1,47 @@
-module.export = [
-    {
-        title: "Gas",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Food",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Lodging",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Entertainment",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Shopping",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Car rental",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Misc.",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-]
+module.export = {
+    userId: null,
+    categories: [
+        {
+            title: "Gas",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+        {
+            title: "Food",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+        {
+            title: "Lodging",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+        {
+            title: "Entertainment",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+        {
+            title: "Shopping",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+        {
+            title: "Car rental",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+        {
+            title: "Misc.",
+            budget: 0,
+            spent: 0,
+            transactions: [],
+        },
+    ],
+}
