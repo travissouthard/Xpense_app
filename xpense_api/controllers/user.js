9a4ba463ad7b54b596e0d4dc9c02c130d72a3438
Travis Southard <tsouthard88@gmail.com>
2020-07-23 10:12:35 -0400

    Made a user/router. Needs authentication and editing to align with user login needs

---
+++ xpense_api/controllers/user.js
@@ -0,0 +1,45 @@
+//DEPENDENCIES
+const express = require("express");
+const user = express.Router();
+
+const User = require("../models/user.js")
+
+//ROUTES
+user.get("/:username", (req, res) => {
+    User.find({username: req.params.username}, (err, foundUser) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(foundUser);
+    });
+});
+
+user.post("/", (req, res) => {
+    User.create(req.body, (err, createdUser) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(createdUser);
+    });
+});
+
+user.put("/:id", (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(updatedUser);
+    });
+});
+
+user.delete("/:id", (req, res) => {
+    User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
+        if (err) {
+            res.status(400).json({"Error": err.message});
+        }
+        res.status(200).json(deletedUser);
+    });
+});
+
+//EXPORTS
+module.exports = users
