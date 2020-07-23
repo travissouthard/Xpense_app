a9f4da5021dfe3fc59edb8c093b77a382c351b45
Travis Southard <tsouthard88@gmail.com>
2020-07-23 11:39:29 -0400

    Put the password encryption into the user create route in controllers/user.js and deleted the extra user

--- xpense_api/controllers/user.js
+++ xpense_api/controllers/user.js
@@ -11,15 +11,6 @@ const User = require("../models/user.js")
 //     res.render('')
 //   })
 
-user.post('/', (req, res) => {
-    //overwrite the user password with the hashed password, then pass that in to our database
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-    User.create(req.body, (err, createdUser) => {
-        console.log('user is created', createdUser)
-        res.redirect('/')
-    })
-})  
-
 user.get("/:username", (req, res) => {
     User.find({username: req.params.username}, (err, foundUser) => {
         if (err) {
@@ -30,6 +21,7 @@ user.get("/:username", (req, res) => {
 });
 
 user.post("/", (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     User.create(req.body, (err, createdUser) => {
         if (err) {
             res.status(400).json({"Error": err.message});
