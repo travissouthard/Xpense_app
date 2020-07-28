e14cb8c935aa92608a9f8e81518aa957af384e67
Travis Southard <tsouthard88@gmail.com>
2020-07-28 09:45:47 -0400

    Adding res.header to allow cors preflight

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ const corsOptions = {
   }
 }
 app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 //Controller/Routes
 const budgetsController = require("./controllers/budget.js");
